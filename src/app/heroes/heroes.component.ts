628028143956bf290780e3a3e2ad33b74be70189
Guillaume Fricker <fricker.g@zoho.com>
2018-07-15 21:55:54 +0200

    Interaction with the selected hero object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,9 +10,14 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 	// initialize the mocked heroes List in class scope
 	heroes = HEROES;
+	selectedHero: Hero;
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
 	}
 }
