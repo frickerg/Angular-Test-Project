7fe8d29e8d55a93afa23a9fad0588abbd8c4fe94
Guillaume Fricker <fricker.g@zoho.com>
2018-07-15 18:11:06 +0200

    Using ngModel FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
@@ -13,6 +14,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 	],
 	imports: [
 		BrowserModule,
+		FormsModule,
 		BrowserAnimationsModule,
 		MatToolbarModule
 	],
