9ba38850e5fb3050f94ac3e1df93f7af5a4561d5
Guillaume Fricker <fricker.g@zoho.com>
2018-07-16 11:31:53 +0200

    Added dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './modules/app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppRoutingModule } from './modules/app-routing.module';
 
 @NgModule({
 	declarations: [
